468e389eb7558346c9ff5a9995a43ad920f71e79
Thano <jonathan.abriol@gmail.com>
2019-08-29 14:44:38 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Welcome to Kinetic</h1>
+    <p>We're here to welcome you.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
